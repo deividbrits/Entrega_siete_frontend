074e0f9e4c0a287bf9515e144d915d7a96db1b1c
deividbrits <dabrito.dwfs13@bootcampudd.cl>
2024-09-14 23:18:19 -0300

    cambios en about us

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Typography } from '@mui/material'
+import otraimagen from '/otraimagen.png'
 
 export const AboutUs = () => {
   return (
     <div className='about-conteiner' >
+      <img src={otraimagen} style={{alignItems : 'center'}}></img>
      
-
-        <Typography variant="h1" sx={{ textAlign: 'center',color: 'white' }}>Hola, somos AllManga  </Typography>
+        <p> <Typography variant="h1" sx={{ textAlign: 'center',color: 'white' }}>Hola, somos AllManga  </Typography></p>
+ 
       <section >
           <div >
+            <p>
           <Typography variant='h2' sx={{color:"#872d16"}}>En este espacio amamos leer , sobre todo acerca de dibujo y animación japonesa, es por eso que 
             queremos acercar a las personas a obtener ese manga que tanto estaban buscando.
           </Typography>
+          </p>
+ 
         </div>
       </section>
     </div>
